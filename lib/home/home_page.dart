4be1a590b5c2881935a0e4bf284fe87b490da3a9
Gustavo Santos <gustavo93santos@gmail.com>
2021-04-22 02:42:23 -0300

    Aula 02 - Maximum Speed - Finalizada

---
+++ lib/home/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:devquiz/home/widgets/appbar/appbar.dart';
+import 'package:devquiz/home/widgets/level_button/level_button_widfet.dart';
+import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBarWidget(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(width: 5,),
+                  LevelButtonWidget(label: "Fácil"),
+                  SizedBox(width: 5,),
+                  LevelButtonWidget(label: "Médio"),
+                  SizedBox(width: 5,),
+                  LevelButtonWidget(label: "Difícil"),
+                  SizedBox(width: 5,),
+                  LevelButtonWidget(label: "Perito"),
+                  SizedBox(width: 20,)
+                 ],
+              ),
+              SizedBox(height: 20),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  children: [
+                    QuizCardWidget(),QuizCardWidget(),QuizCardWidget(),QuizCardWidget()
+                  ],
+                ),
+              ) 
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
