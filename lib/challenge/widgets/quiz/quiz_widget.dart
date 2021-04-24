1e6b19b2c3e231c26da6455efe610c34b5ea8a23
Gustavo <gustavo93santos@gmail.com>
2021-04-24 04:48:05 -0300

    Aula 04 - Landing

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,10 +1,11 @@
+import 'package:devquiz/shared/models/answer_model.dart';
 import 'package:flutter/material.dart';
 
 import 'package:devquiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:devquiz/core/core.dart';
 import 'package:devquiz/shared/models/question_model.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
   const QuizWidget({
     Key? key,
@@ -12,22 +13,30 @@ class QuizWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+  AnswerMoldel answeres(int index) => widget.question.answers[index];
+  @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-          SizedBox(height: 64,),
+          SizedBox(
+            height: 64,
+          ),
           Text(
-            question.title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(height: 24),
-          ...question.answers.map(
-            (e) => AnswerWidget(
-              isRight: e.isRight,
-              title: e.title,
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              isRight: answeres(i).isRight,
+              title: answeres(i).title,
             ),
-          ).toList(),
         ],
       ),
     );
