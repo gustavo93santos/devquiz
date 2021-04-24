f5c84ad4e2a3a9630c5994181cf15df271709365
Gustavo <gustavo93santos@gmail.com>
2021-04-24 03:17:34 -0300

    Aula 04 - Landing

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:devquiz/challenge/challenge_controller.dart';
 import 'package:flutter/material.dart';
 
 import 'package:devquiz/challenge/widgets/next_button/next_button_widget.dart';
@@ -17,7 +18,19 @@ class ChallengePage extends StatefulWidget {
 }
 
 class _ChallengePageState extends State<ChallengePage> {
+  final controller = ChallengeController();
+  final pageController = PageController();
   @override
+  void initState() {
+    controller.currentPageNotifier.addListener(() {
+      setState(() {});
+    });
+    pageController.addListener(() {
+      controller.currentPage = pageController.page!.toInt();
+    });
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
@@ -28,11 +41,17 @@ class _ChallengePageState extends State<ChallengePage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 BackButton(),
-                QuestionIndicatorWidget(),
+                QuestionIndicatorWidget(
+                  currentPage: controller.currentPage,
+                  length: widget.questions.length,
+                ),
               ],
             )),
       ),
-      body: QuizWidget(question: widget.questions[0],),
+      body: PageView(
+        controller: PageController(),
+        children: widget.questions.map((e) => QuizWidget(question: e)).toList(),
+      ),
       bottomNavigationBar: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
